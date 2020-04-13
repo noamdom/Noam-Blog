e51708eca953596d5759837493a581aec3da4e24
Noam Domovich <ndomovich@gmail.com>
2020-04-13 17:08:59 +0300

    First Commit

---
+++ src/build/precache-manifest.4602613c3b361caef3fd8f339bcaaa24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aacec8aaae710e70a1c4",
+    "url": "/static/css/main.8e7e6bde.chunk.css"
+  },
+  {
+    "revision": "aacec8aaae710e70a1c4",
+    "url": "/static/js/main.00081d84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "63637c22021a0c80398b556e8e274871",
+    "url": "/index.html"
+  }
+];
